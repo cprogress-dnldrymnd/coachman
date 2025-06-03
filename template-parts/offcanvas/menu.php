758b075dc62d9ce085e4f96668affffc7fafbe31
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-06-03 20:14:26 +0800

    css

--- template-parts/offcanvas/menu.php
+++ template-parts/offcanvas/menu.php
@@ -1,9 +1,18 @@
-<div class="offcanvas offcanvas-end" tabindex="-1" id="offCanvasMenu"
+<div class="offcanvas offcanvas-start" tabindex="-1" id="offCanvasMenu"
     aria-labelledby="offCanvasMenuLabel">
     <div class="offcanvas-body">
         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         <div class="offcanvas-body--inner background-white rounded overflow-hidden">
-        
+            <div class="container">
+                <div class="row g-0">
+                    <div class="col-lg-6">
+
+                    </div>
+                    <div class="col-lg-6">
+
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
