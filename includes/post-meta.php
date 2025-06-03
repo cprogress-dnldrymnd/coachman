254487428c14c6df3ca90dc969edfd2ae8aef79a
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-06-03 20:22:50 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -131,7 +131,7 @@ Block::make(__('Tabs Navigation'))
         }
         ?>
         <div class="<?= $class1 ?> overflow-visible sm-margin-bottom">
-            <ul class="<?= $class12 ?> swiper-wrapper nav nav-tabs" id="<?= $fields['tab_id'] ?>" role="tablist">
+            <ul class="<?= $class2 ?> swiper-wrapper nav nav-tabs" id="<?= $fields['tab_id'] ?>" role="tablist">
                 <?= $inner_blocks ?>
             </ul>
         </div>
