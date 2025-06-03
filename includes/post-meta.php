b6dce44b553867285861cb5cfd1ffee48aed1bc3
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-06-03 19:58:51 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -360,8 +360,11 @@ Block::make(__('Caravan/Motohomes Models'))
                                             <?= wp_get_attachment_image($image, 'medium') ?>
                                         </div>
                                     <?php } ?>
-                                    <div class="model-num">
-                                        <?= count($posts_listings) ?> Models
+                                    <div class="model-num d-flex gap-2 align-items-center justify-content-between">
+                                        <span> <?= count($posts_listings) ?> Models</span>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+                                            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
+                                        </svg>
                                     </div>
                                 </div>
                             </div>
