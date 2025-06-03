71d0800e8773e8f6ebc489e84fcc4da89e3b159e
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-06-03 20:42:26 +0800

    Update menu.php

--- template-parts/offcanvas/menu.php
+++ template-parts/offcanvas/menu.php
@@ -1,6 +1,6 @@
 <div class="offcanvas offcanvas-start" tabindex="-1" id="offCanvasMenu"
     aria-labelledby="offCanvasMenuLabel">
-    <div class="offcanvas-body">
+    <div class="offcanvas-body p-0">
         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         <div class="offcanvas-body--inner background-white rounded overflow-hidden">
             <?php
