ad85abbd6c13383de71f019fe6e11009dcb7cc42
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-06-04 15:39:15 +0800

    Update listing-functions.php

--- includes/listing-functions.php
+++ includes/listing-functions.php
@@ -424,7 +424,7 @@ function __listing_buttons($post_id)
     $_360_walkthrough = get__post_meta_by_id($post_id, '360_walkthrough');
     $video = get__post_meta_by_id($post_id, 'video');
 ?>
-    <div class="listing--buttons">
+    <div class="listing--buttons mt-2">
         <ul class="d-flex gap-3 m-0 fs-15 p-0 w-100 justify-content-between align-items-center list-inline">
             <?php if ($_360_walkthrough) { ?>
                 <li>
