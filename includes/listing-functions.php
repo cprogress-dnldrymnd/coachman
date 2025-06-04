df6048532f3598489b44a036d2298ccaf00308ca
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-06-04 15:42:53 +0800

    Update listing-functions.php

--- includes/listing-functions.php
+++ includes/listing-functions.php
@@ -455,6 +455,19 @@ function __listing_buttons($post_id)
             </div>
         </div>
     </div>
+    <div class="offcanvas offcanvas--layouts offcanvas-end" tabindex="-1" id="offCanvasVideo-<?= $post_id ?>" aria-labelledby="offCanvasVideo-<?= $post_id ?>Label" aria-modal="true" role="dialog">
+        <div class="offcanvas-body p-0 overflow-hidden">
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                    <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"></path>
+                </svg>
+            </button>
+            <div class="offcanvas-body--inner background-white rounded overflow-hidden">
+                <p class="fs-22">Video Tour</p>
+                <iframe src="<?= $video ?>" frameborder="0"></iframe>
+            </div>
+        </div>
+    </div>
 <?php
     return ob_get_clean();
 }
