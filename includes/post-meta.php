bee784a5dbefaecc46b036a7cdaab88095c1035d
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-06-05 15:38:11 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -271,7 +271,7 @@ Block::make(__('Swiper'))
         $atts_json = json_encode($atts);
 ?>
     <div class="swiper-slider-holder" <?= $attributes['className'] ?> <?= $atts_json ?>>
-        <div class="swiper swiper-slider-block">
+        <div class="swiper swiper-slider-block" <?= $swiper_id ?>>
             <?= $inner_blocks ?>
         </div>
     </div>
