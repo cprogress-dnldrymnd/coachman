4478dfdbce8e719c4e390c95074435a7f6c66d3e
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-07-11 23:25:27 +0800

    Update page.php

--- page.php
+++ page.php
@@ -3,12 +3,12 @@
 <div class="site-content site-content--default md-padding-top md-padding-bottom">
     <?php while (have_posts()) { ?>
         <div class="title-description py-5">
-            <div class="container">
+            <div class="container small-container">
                 <h1 class="m-0"><?php the_title() ?></h1>
             </div>
         </div>
-        <div class="site-content--inner background-white rounded overflow-hidden has-lightgray-2-background-color">
-            <div class="container">
+        <div class="site-content--inner background-white rounded overflow-hidden has-lightgray-2-background-color md-padding-top md-padding-bottom">
+            <div class="container small-container">
                 <?php the_post() ?>
                 <?php the_content() ?>
             </div>
