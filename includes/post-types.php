abbd4b532bb19441d8480ecae04903d43905aba6
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-07-11 21:10:26 +0800

    Update post-types.php

--- includes/post-types.php
+++ includes/post-types.php
@@ -326,6 +326,43 @@ $Careers->show_in_admin_bar = false;
 $Careers->has_archive = false;
 
 
+
+$Careers_Category = new newTaxonomy();
+$Careers_Category->taxonomy = 'careers_category';
+$Careers_Category->post_type = 'Careers';
+$Careers_Category->args = array(
+    'label'        => 'Careers Category',
+    'labels' => array(
+        'name'                       => _x('Careers Category', 'Taxonomy General Name', 'text_domain'),
+        'singular_name'              => _x('Careers Category', 'Taxonomy Singular Name', 'text_domain'),
+        'menu_name'                  => __('Careers Category', 'text_domain'),
+        'all_items'                  => __('All Careers Category', 'text_domain'),
+        'parent_item'                => __('Parent Careers Category', 'text_domain'),
+        'parent_item_colon'          => __('Parent Item:', 'text_domain'),
+        'new_item_name'              => __('New Item Name', 'text_domain'),
+        'add_new_item'               => __('Add New Item', 'text_domain'),
+        'edit_item'                  => __('Edit Item', 'text_domain'),
+        'update_item'                => __('Update Item', 'text_domain'),
+        'view_item'                  => __('View Item', 'text_domain'),
+        'separate_items_with_commas' => __('Separate items with commas', 'text_domain'),
+        'add_or_remove_items'        => __('Add or remove Careers Category', 'text_domain'),
+        'choose_from_most_used'      => __('Choose from the most used', 'text_domain'),
+        'popular_items'              => __('Popular Careers Category', 'text_domain'),
+        'search_items'               => __('Search Careers Category', 'text_domain'),
+        'not_found'                  => __('Not Found', 'text_domain'),
+        'no_terms'                   => __('No Careers Category', 'text_domain'),
+        'items_list'                 => __('Items list', 'text_domain'),
+        'items_list_navigation'      => __('Items list navigation', 'text_domain'),
+    ),
+    'hierarchical' => true,
+    'query_var'    => true,
+    'show_in_rest' => true,
+    'rewrite'      => array(
+        'slug'         => 'careers-category',
+    )
+);
+
+
 $Downloads = new newPostType();
 $Downloads->key = 'downloads';
 $Downloads->name = 'Downloads';
