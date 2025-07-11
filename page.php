ee2afa75e0e7b173fdff16b5239523d2e5f836e0
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-07-11 23:27:36 +0800

    css

--- page.php
+++ page.php
@@ -1,13 +1,13 @@
 <?php get_header() ?>
 
-<div class="site-content site-content--default md-padding-top md-padding-bottom">
+<div class="site-content site-content--default">
     <?php while (have_posts()) { ?>
         <div class="title-description py-5">
             <div class="container small-container">
                 <h1 class="m-0"><?php the_title() ?></h1>
             </div>
         </div>
-        <div class="site-content--inner background-white rounded overflow-hidden has-lightgray-2-background-color md-padding-top md-padding-bottom default-template">
+        <div class="site-content--inner md-padding-top md-padding-bottom overflow-hidden has-lightgray-2-background-color md-padding-top md-padding-bottom default-template">
             <div class="container small-container">
                 <?php the_post() ?>
                 <?php the_content() ?>
