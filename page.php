723af58ceb223aaebf82521c8a9c461aee9d2a2d
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-07-11 23:24:36 +0800

    Update page.php

--- page.php
+++ page.php
@@ -1,15 +1,19 @@
 <?php get_header() ?>
 
 <div class="site-content site-content--default md-padding-top md-padding-bottom">
-    <div class="container">
-        <?php while (have_posts()) { ?>
-            <h1 class="fs-35 sm-margin-bottom"><?php the_title() ?></h1>
-            <div class="site-content--inner background-white rounded overflow-hidden has-lightgray-2-background-color">
+    <?php while (have_posts()) { ?>
+        <div class="title-description py-5">
+            <div class="container">
+                <h1 class="fs-35 mp-"><?php the_title() ?></h1>
+            </div>
+        </div>
+        <div class="site-content--inner background-white rounded overflow-hidden has-lightgray-2-background-color">
+            <div class="container">
                 <?php the_post() ?>
                 <?php the_content() ?>
             </div>
-        <?php } ?>
-    </div>
+        </div>
+    <?php } ?>
 </div>
 
 <?php get_footer() ?>
