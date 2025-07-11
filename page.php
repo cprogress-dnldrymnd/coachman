d1ef858c03f8679c3422fe6e893d1c566c672de0
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-07-11 23:23:45 +0800

    cs

--- page.php
+++ page.php
@@ -1,10 +1,10 @@
 <?php get_header() ?>
 
-<div class="site-content site-content--default md-padding-top md-padding-bottom background-light-gray-4">
+<div class="site-content site-content--default md-padding-top md-padding-bottom">
     <div class="container">
         <?php while (have_posts()) { ?>
             <h1 class="fs-35 sm-margin-bottom"><?php the_title() ?></h1>
-            <div class="site-content--inner background-white p-5 rounded overflow-hiddens">
+            <div class="site-content--inner background-white rounded overflow-hidden has-lightgray-2-background-color">
                 <?php the_post() ?>
                 <?php the_content() ?>
             </div>
