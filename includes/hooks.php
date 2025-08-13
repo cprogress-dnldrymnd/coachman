ffb7647fbf8f656eab4365522dc27138e3b3b126
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-08-13 21:33:59 +0800

    Update hooks.php

--- includes/hooks.php
+++ includes/hooks.php
@@ -146,11 +146,12 @@ function my_custom_disable_autop_for_post_type($content)
 {
     // Specify the post type you want to target.
     // Replace 'your_post_type' with the slug of your custom post type.
-    $target_post_type = 'template';
+    $target_post_type = '';
 
     // Check if we are on a single post of the target post type.
     // is_singular() is a good check for both posts and pages.
-    if (is_singular($target_post_type)) {
+    global $post;
+    if ($post->post_type == 'template') {
         // If it's the target post type, remove the wpautop filter.
         remove_filter('the_content', 'wpautop');
         // You can also remove the shortcode formatting filter if needed.
