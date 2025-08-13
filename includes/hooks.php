79cbe8b82d585b638726d8dc4d213ead40242d59
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-08-13 21:29:28 +0800

    Update hooks.php

--- includes/hooks.php
+++ includes/hooks.php
@@ -135,9 +135,7 @@ function prevent_term_deletion($term, $taxonomy)
 }
 
 function remove_wpautop_for_post_type() {
-    // Check if we are on a single post page and if it's the specific post type
-    if ( get_post_type() == 'template' ) {
-        remove_filter( 'the_content', 'wpautop' );
-    }
+           remove_filter( 'the_content', 'wpautop' );
+
 }
 add_action( 'init', 'remove_wpautop_for_post_type' );
