8492ef48ca76dda2270810154ce58a1388ec56b6
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-08-13 21:51:25 +0800

    cs

--- template-parts/shortcodes/dealer-locator.php
+++ template-parts/shortcodes/dealer-locator.php
@@ -30,7 +30,15 @@ $category = isset($_GET['category']) ? sanitize_text_field($_GET['category']) : 
 <script>
     jQuery(document).ready(function() {
         jQuery('body').on('click', '.btn-appointment a', function() {
-           console.log('test');
+            $originalText = jQuery('.request--appointment--dealer h5').text();
+            $dealerName = jQuery(this).closest('h4').text();
+            jQuery('.request--appointment--dealer h5').text($originalText.replace('[dealer_name]', $dealerName));
+            $new_text = $originalText.replace('[dealer_name]', $dealerName);
+
+
+            jQuery(this).closest('h4').text('Appointment with ' + $new_text);
+
         });
     });
+
 </script>
