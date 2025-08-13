6820903cd3bc5974cc60c26c29cfa526b93a6388
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-08-13 22:33:38 +0800

    Update ajax.php

--- includes/ajax.php
+++ includes/ajax.php
@@ -14,11 +14,41 @@ function dealer_details_ajax()
 
 	var_dump($stocks);
 	echo '</pre>';
+	$stocks_ids = [];
+	foreach ($stocks as $stock) {
+		$stocks_ids[] = $stock['id'];
+	}
+
+	$args = array(
+		'post_type' => array('motorhome', 'caravan'),
+		'numberposts' => -1,
+		'include' => $stocks_ids
+
+	);
+	$posts_listings = get_posts($args);
 ?>
 	<div class="dealer--details">
 		<h3 class="mb-4"><?= $_post->post_title ?></h3>
 		<div class="dealer--desc">
 			<?= $_post->post_content ?>
+		</div>
+
+		<div class="listings--posts bg-lightgray-2" id="listings--posts-<?= $key ?>-<?= $post['_type'] ?>-<?= $model ?>">
+			<div class="container  py-5">
+				<div class="row g-3">
+					<?php foreach ($posts_listings as $posts_listing) { ?>
+						<div class="col-lg-3">
+							<div class="listings--posts--grid bg-white p-4">
+								<h3 class="fs-24"><?= __listing_title($posts_listing->ID) ?></h3>
+								<div class="image-box image-style image-style-2 mb-3" style="--fit: contain">
+									<?= get_the_post_thumbnail($posts_listing->ID, 'medium') ?>
+								</div>
+								<?= __listing_features($posts_listing->ID) ?>
+							</div>
+						</div>
+					<?php } ?>
+				</div>
+			</div>
 		</div>
 	</div>
 <?php
