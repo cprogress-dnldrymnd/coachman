08b0df392de7a01efa7cd48d536cd9bf78ad61cc
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-08-13 22:22:58 +0800

    Update ajax.php

--- includes/ajax.php
+++ includes/ajax.php
@@ -4,10 +4,13 @@ add_action('wp_ajax_dealer_details_ajax', 'dealer_details_ajax');
 function dealer_details_ajax()
 {
 	$post_id = $_POST['post_id'];
-	$_post = get_post($post_id)
+	$_post = get_post($post_id);
+	echo '<pre>';
+	var_dump(get_post_meta($post_id));
+	echo '</pre>';
 ?>
 	<div class="dealer--details">
-		<h3><?= $_post->post_title ?></h3>
+		<h3 class="mb-4"><?= $_post->post_title ?></h3>
 		<div class="dealer--desc">
 			<?= $_post->post_content ?>
 		</div>
