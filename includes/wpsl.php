8c13fdc9e32a0422e6f1ac83eca1829e8918d724
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-08-13 21:12:07 +0800

    Update wpsl.php

--- includes/wpsl.php
+++ includes/wpsl.php
@@ -62,7 +62,7 @@ function custom_listing_template()
         $listing_template .= "\t\t" . '<div class="distance"><svg xmlns="http://www.w3.org/2000/svg" width="31.226" height="41.617" viewBox="0 0 31.226 41.617"> <path id="pin-coachman" d="M4,15.606A15.421,15.421,0,0,1,11.785,2.123,15.76,15.76,0,0,1,19.609,0a14.689,14.689,0,0,1,7.824,2.122,16.149,16.149,0,0,1,5.7,5.66,15,15,0,0,1,2.081,7.824,12.29,12.29,0,0,1-.874,4.162,34.891,34.891,0,0,1-2.206,4.786Q30.8,26.967,29.1,29.464t-3.329,4.661q-1.623,2.164-3.038,3.829t-2.247,2.705l-.874.957q-.333-.333-.874-1T16.53,38q-1.665-1.956-3.08-3.912t-3.288-4.578a39.468,39.468,0,0,1-3.08-4.994q-1.207-2.372-2.206-4.7A9.223,9.223,0,0,1,4,15.606Zm5.2,0a10.031,10.031,0,0,0,3.038,7.366,10.031,10.031,0,0,0,7.366,3.038,10.031,10.031,0,0,0,7.366-3.038,10.031,10.031,0,0,0,3.038-7.366,9.894,9.894,0,0,0-3.038-7.324A10.4,10.4,0,0,0,19.609,5.2a9.562,9.562,0,0,0-7.366,3.08A10.255,10.255,0,0,0,9.205,15.606Z" transform="translate(-3.989 0.001)" fill="currentColor"/> </svg> <%= distance %> ' . esc_html($wpsl_settings['distance_unit']) . '</div>' . "\r\n";
     }
     $listing_template .= "<div class='listing--buttons'>";
-    $listing_template .= "<div class='btn btn-appointment'><a>Request Appointment</a></div>";
+    $listing_template .= "<div class='btn btn-appointment'><a data-bs-toggle='offcanvas' data-bs-target='#offCanvas25605' aria-controls='offCanvas25605'>Request Appointment</a></div>";
     $listing_template .= "\t\t" . '<div class="btn btn-direction"><%= createDirectionUrl() %></div>' . "\r\n";
     $listing_template .= "<div class='btn btn-stock'><a>View Stock</a></div>";
     $listing_template .= "</div>";
