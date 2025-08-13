2aa5db645e2d0fafb8259ae30d0b1fdb717ce3b8
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-08-13 18:20:42 +0800

    Update dealer-locator.php

--- template-parts/shortcodes/dealer-locator.php
+++ template-parts/shortcodes/dealer-locator.php
@@ -1,19 +1,17 @@
-<div class="swiper swiper-nav-tabs-swiper nav-tabs-swiper overflow-visible sm-margin-bottom nav-tabs-swiper-js swiper-initialized swiper-horizontal swiper-free-mode swiper-backface-hidden">
-    <ul class="swiper-wrapper nav nav-tabs  flex-row " id="Dealers" role="tablist" aria-live="polite">
-        <li class="swiper-slide nav-item">
-            <a class="nav-link">
-                <p>Caravan Dealers</p>
-            </a>
-        </li>
-        <li class="swiper-slide nav-item">
-            <a class="nav-link">
-                <p>Motorhome Dealers</p>
-            </a>
-        </li>
-        <li class="swiper-slide">
-            <a class="nav-link">
-                <p>Imperial Dealers</p>
-            </a>
-        </li>
+<?php
+$dealer_cat = get_terms(array(
+    'taxonomy' => 'wpsl_store_category',
+    'hide_empty' => false,
+));
+?>
+<div class="swiper swiper-nav-tabs-swiper nav-tabs-swiper overflow-visible sm-margin-bottom nav-tabs-swiper-js">
+    <ul class="swiper-wrapper nav nav-tabs  flex-row " id="Dealers-Navigation" role="tablist" aria-live="polite">
+        <?php foreach ($dealer_cat as $dealer) { ?>
+            <li class="swiper-slide nav-item">
+                <a class="nav-link" href="?category=<?= $dealer->slug ?>">
+                    <p><?= $dealer->name ?></p>
+                </a>
+            </li>
+        <?php } ?>
     </ul>
 </div>
