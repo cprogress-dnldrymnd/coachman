fcebafd2363a95b57c39b3090ba2be5891dbbdca
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-08-13 23:34:59 +0800

    Update dealer-locator.php

--- template-parts/shortcodes/dealer-locator.php
+++ template-parts/shortcodes/dealer-locator.php
@@ -23,13 +23,13 @@ $category = isset($_GET['category']) ? sanitize_text_field($_GET['category']) : 
     </div>
 
     <div class="dealer--locator">
-        <?= do_shortcode('[wpsl template="default" category="' . $category . '" start_location="RH20 3BT"]') ?>
+        <?= do_shortcode('[wpsl template="default" category="' . $category . '"]') ?>
     </div>
 </div>
 <?php if (isset($_GET['wpsl-search-input'])) { ?>
     <script>
         jQuery(document).ready(function() {
-
+            jQuery('input[name="wpsl-search-input"]').val('<?= $_GET['wpsl-search-input'] ?>');
         });
     </script>
 
