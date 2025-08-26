bdb23979ea5c33182dc1d2e97100dd5c0f5f414e
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-08-26 14:11:25 +0800

    Update single.php

--- single.php
+++ single.php
@@ -1,8 +1,3 @@
 <?php get_header() ?>
 <?= do_shortcode('[template template_id=26193]') ?>
-<section class="archive-courses archive-grid background-light-gray py-5">
-    <div class="container large-container">
-        <?php the_content() ?>
-    </div>
-</section>
 <?php get_footer() ?>
