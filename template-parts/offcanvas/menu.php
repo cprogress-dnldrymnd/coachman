9bb86242b639ac2dc434f8c68e1cc3e80836486a
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-08-27 12:59:45 +0800

    css

--- template-parts/offcanvas/menu.php
+++ template-parts/offcanvas/menu.php
@@ -6,7 +6,7 @@
                 <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
             </svg>
         </button>
-        <div class="offcanvas-body--inner background-white rounded overflow-hidden">
+        <div class="offcanvas-body--inner background-white  overflow-hidden">
             <?php
             echo do_shortcode('[template template_id=25503]');
             ?>
