a0ee7014d4045ddb69a76770c84800b7089f3964
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-09-02 14:15:34 +0800

    Update listing-functions.php

--- includes/listing-functions.php
+++ includes/listing-functions.php
@@ -209,13 +209,12 @@ function specifications($post_id)
 function specification($post_id, $meta_key, $label)
 {
     $meta = get__post_meta_by_id($post_id, $meta_key);
-    $currency = '';
+    if ($meta_key == 'price') {
+        $meta = '£' . $meta;
+    }
     if ($meta) {
         $meta = wpautop($meta);
-        if ($meta_key == 'price') {
-            $currency = '£';
-        }
-        return "<div class='specification'><div class='meta-label'><strong>$label</strong></div><div class='meta-value'>$currency $meta</div></div>";
+        return "<div class='specification'><div class='meta-label'><strong>$label</strong></div><div class='meta-value'> $meta</div></div>";
     }
 }
 /**
