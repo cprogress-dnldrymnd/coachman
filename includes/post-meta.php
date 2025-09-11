86e1f394c740c89fbd6385b8df33dfaf2a96e443
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-09-11 19:40:45 +0800

    cs

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -496,7 +496,7 @@ Block::make(__('Caravan/Motohomes Models'))
                                         </div>
                                     <?php } ?>
                                     <div class="model-num d-flex gap-2 align-items-center justify-content-between fs-15">
-                                        <span> <?= count($posts_listings) ?> Models</span>
+                                        <span> <?= count($posts_listings) ?> Model<?= count($posts_listings) > 1 ? 's' : '' ?></span>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
                                             <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
                                         </svg>
