b2660a02d8f996c8ee48159f4ff4f8fc465aae4e
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-09-17 21:52:56 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -298,6 +298,11 @@ function query_loop_block_query_vars__artist($query, $block)
 {
 
     if (get_the_ID() == 123) {
+        $query['meta_key'] = '_event_date';
+        $query['orderby'] = 'meta_value';
+        $query['order'] = 'desc';
+        $query['meta_type'] = 'DATE';
+        
         
     }
     return $query;
