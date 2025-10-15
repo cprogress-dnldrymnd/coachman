a8e02ae36d7cbee64f58c2b9c0c37a9adc8128f1
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-10-16 01:05:58 +0800

    Update post-meta.php

--- includes/post-meta.php
+++ includes/post-meta.php
@@ -564,7 +564,6 @@ Block::make(__('Caravan/Motohomes Models'))
                             <?php } ?>
                         </div>
                         <?php if ($post['_type'] == 'caravan') { ?>
-                            <?= $key ?>
                             <p class="otr-price mt-4">
                                 *All prices exclude a recommended dealer charge of £650 (including VAT). Prices effective from 1st September 2024.
                             </p>
