a4896256a293a896fca6dbb5d9578e3e8bdfee8e
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-11-19 23:23:09 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
                 <?php while (have_posts()) { ?>
                     <?php the_post() ?>
                     <div class="col-md-4 col-6">
-                        <?= do_shortcode('[template id=25081]') ?>
+                        <?= do_shortcode('[template template_id=25081]') ?>
                     </div>
                 <?php } ?>
             </div>
