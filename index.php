80efd04a855d197e6cdef2a20ae6938a7a0efa84
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-11-19 23:26:21 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php get_header() ?>
 <section class="py-5">
     <div class="container">
+        <h1><?= get_the_archive_title() ?></h1>
         <?= do_shortcode('[template template_id=28166]') ?>
     </div>
 </section>
