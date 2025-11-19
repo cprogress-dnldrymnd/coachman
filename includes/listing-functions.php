8a7b76e52df5ea095c6f306829154933294fe3d4
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-11-19 17:59:27 +0800

    Update listing-functions.php

--- includes/listing-functions.php
+++ includes/listing-functions.php
@@ -156,7 +156,7 @@ function __listing_buttons($post_id)
                         <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"></path>
                     </svg>
                 </button>
-                <div class="offcanvas-body--inner background-white rounded overflow-hidden p-3 p-lg-5" <?=  $video ?>>
+                <div class="offcanvas-body--inner background-white rounded overflow-hidden p-3 p-lg-5" <?= $video ?>>
                     <h2 class="fs-24"><?= __listing_title(get_the_ID()) ?></h2>
                     <p class="fs-22">Range Tour</p>
                     <div class="embed-holder position-relative mb-5">
@@ -226,7 +226,7 @@ function specifications($post_id)
 
     <?php if (get_post_type($post_id) == 'caravan') { ?>
         <p class="otr-price mt-4">
-            *All prices exclude a recommended dealer charge of £650 (including VAT). Prices effective from 1st September 2024.
+            *All prices exclude a recommended dealer charge of £650 (including VAT). Prices effective from 14th August 2025.
         </p>
     <?php } else { ?>
         <p class="otr-price mt-4">
@@ -283,7 +283,7 @@ function getYoutubeEmbedUrl($url)
 {
     // A regular expression to match and capture the video ID from various YouTube URL formats.
     // The pattern looks for 'v=', '/embed/', or '.be/' followed by an 11-character video ID.
-    $pattern = '/(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:watch|embed)\/|v\/)|youtu\.be\/)([a-zA-Z0-9_-]{11})/';
+    $pattern = '/(?:https?://)?(?:www.)?(?:youtube.com/(?:[^/ns]+/S+/|(?:watch|embed)/|v/)|youtu.be/)([a-zA-Z0-9_-]{11})/';
 
     // Use preg_match to find the pattern in the provided URL.
     if (preg_match($pattern, $url, $matches)) {
