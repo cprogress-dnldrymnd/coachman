16b6566e7e18b1da6aa53b365f3c8161180d2928
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-11-19 23:25:21 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,4 @@
 <?php get_header() ?>
-
-<section class="archive-courses archive-grid background-light-gray py-5">
-    <div class="container large-container">
-        <?php if (have_posts()) { ?>
-            <div class="row">
-                <?php while (have_posts()) { ?>
-                    <?php the_post() ?>
-                    <div class="col-md-4 col-6">
-                        <?= do_shortcode('[template template_id=25081]') ?>
-                    </div>
-                <?php } ?>
-            </div>
-        <?php } else { ?>
-            <div class="heading-box">
-                <h2>
-                    No results found.
-                </h2>
-            </div>
-        <?php } ?>
-    </div>
-</section>
+ <?= do_shortcode('[template template_id=28166]') ?>
 
 <?php get_footer() ?>
