07adf46fae59ffb1920ab420055aacd7e27bb3de
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-11-19 23:26:00 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php get_header() ?>
- <?= do_shortcode('[template template_id=28166]') ?>
+<section class="py-5">
+    <div class="container">
+        <?= do_shortcode('[template template_id=28166]') ?>
+    </div>
+</section>
 
 <?php get_footer() ?>
