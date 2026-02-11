f0a3495a5549e60d2b4c3d3a91a7bda2b3011763
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-02-11 19:08:19 +0800

    Update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -134,4 +134,4 @@ add_shortcode('pdf_file', 'pdf_file');
 function motorhome_text() {
     return get__theme_option('motorhome_text');
 }
-add_shortcode($'motorhome_text', 'motorhome_text');
+add_shortcode('motorhome_text', 'motorhome_text');
